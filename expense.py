6da5dcf3cb7271c44b236bb4139956208590d3be
Maxime Descouts <maxime.descouts@epita.fr>
2022-11-02 21:24:22 +0100

    involved: done

--- expense.py
+++ expense.py
@@ -33,6 +33,33 @@ def new_expense(*args):
 
     infos = prompt(expense_questions)
 
+    involedChoices = []
+    spender = infos['spender']
+
+    for user in users:
+        if spender == user :
+            involedChoices.append({
+                'name' : user,
+                'checked': 'true',
+                'disabled': 'true'
+            })
+        else :
+            involedChoices.append(dict(zip(['name'], [user])))
+
+    #print("mapped involved users liss : " + str(involedChoices))
+
+    involved = prompt(
+        {
+            "type":"checkbox",
+            "name":"involved",
+            "message":"New Expense - Involved Users: ",
+            "choices": involedChoices
+        },
+    )
+
+    print(involved)
+    infos.update(involved)
+
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
     with open('expense_report.csv', 'a', newline='') as file:
         writer = csv.writer(file)
